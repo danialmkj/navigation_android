9ae435cf2d02c5286798291cf464149d3f528315
MohammaddMo <mohammadd.moazami007@gmail.com>
2025-01-13 15:56:39 +0330

    initial commit

---
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationSampleTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun MainPreview() {
+    MainActivity()
+}
+
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "firstScreen") {
+        composable("firstScreen") {
+            FirstScreen {
+                navController.navigate("secondScreen")
+            }
+        }
+        composable("secondScreen") {
+            SecondScreen {
+                navController.navigate("firstScreen")
+            }
+        }
+    }
+
+}
